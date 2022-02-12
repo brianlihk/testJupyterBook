0c1344c5c58166601a046da541195ec3817c0746
Brian Li <y171100@yahoo.co.jp>
2022-02-12 17:40:16 +0800

    added some new tests

---
+++ _build/html/_sources/week3/dataProcess-7.md
@@ -0,0 +1,65 @@
+---
+jupytext:
+  formats: md:myst
+  text_representation:
+    extension: .md
+    format_name: myst
+kernelspec:
+  display_name: Python 3
+  language: python
+  name: python3
+---
+# Chart.js with Jupyter book - 6
+
+## Test with random data generation
+
+<div>
+  <canvas id="addableChart"></canvas>
+</div>
+Max length: <input id="input" value=10 />
+
+<script>
+  function addDigit(){
+    console.log(myChart.data.labels.length > inputValue, myChart.data.labels.length, inputValue)
+    if(myChart.data.labels.length > inputValue){
+      myChart.data.datasets[0].data.shift();
+      myChart.data.labels.shift();
+      console.log(myChart.data.labels);
+    }
+    myChart.data.datasets[0].data.push(getRandomInt(0,50));
+    myChart.data.labels.push(myChart.data.labels.length+1);
+    var inputValue = parseInt(document.getElementById('input').value);
+    myChart.update();
+  }
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+
+  const labels = [
+    1,2,3,4,5,6,7
+  ];
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'Test dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {}
+  };
+  const myChart = new Chart(
+    document.getElementById('addableChart'),
+    config
+  );
+
+  setInterval(addDigit, 1000);
+</script>
---
+++ _build/jupyter_execute/week3/dataProcess-6.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Chart.js with Jupyter book - 5
+# 
+# ## Testing on dynamic data
+# ```
+# <div>
+#   <canvas id="addableChart"></canvas>
+# </div>
+# 
+# <script>
+#   const labels = [
+#     1,2,3,4,5,6,7
+#   ];
+# 
+#   const data = {
+#     labels: labels,
+#     datasets: [{
+#       label: 'Test dataset',
+#       backgroundColor: 'rgb(255, 99, 132)',
+#       borderColor: 'rgb(255, 99, 132)',
+#       data: [0, 10, 5, 2, 20, 30, 45],
+#     }]
+#   };
+# 
+#   const config = {
+#     type: 'line',
+#     data: data,
+#     options: {}
+#   };
+# </script>
+# 
+# <script>
+#   const myChart = new Chart(
+#     document.getElementById('addableChart'),
+#     config
+#   );
+# </script>
+# <script>
+# function addDigit(){
+#   myChart.data.labels.push(myChart.data.labels.length+1);
+#   myChart.data.datasets[0].data.push(0);
+#   myChart.update();
+# }
+# </script>
+# <button id='btn' onClick='addDigit()'>Add</button>
+# 
+# ```
+# <div>
+#   <canvas id="addableChart"></canvas>
+# </div>
+# 
+# <script>
+#   const labels = [
+#     1,2,3,4,5,6,7
+#   ];
+# 
+#   const data = {
+#     labels: labels,
+#     datasets: [{
+#       label: 'Test dataset',
+#       backgroundColor: 'rgb(255, 99, 132)',
+#       borderColor: 'rgb(255, 99, 132)',
+#       data: [0, 10, 5, 2, 20, 30, 45],
+#     }]
+#   };
+# 
+#   const config = {
+#     type: 'line',
+#     data: data,
+#     options: {}
+#   };
+# </script>
+# 
+# <script>
+#   const myChart = new Chart(
+#     document.getElementById('addableChart'),
+#     config
+#   );
+# </script>
+# <script>
+# function addDigit(){
+#   myChart.data.labels.push(myChart.data.labels.length+1);
+#   myChart.data.datasets[0].data.push(0);
+#   myChart.update();
+# }
+# </script>
+# <button id='btn' onClick='addDigit()'>Add</button>
